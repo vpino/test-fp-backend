7baa994bee001050671767b1965879ba0aa245e7
Victor Pino <vicpino.dev@gmail.com>
2024-06-16 10:31:35 -0400

    feat(src): update iam role

--- terraform/fuap-backend/iam.tf
+++ terraform/fuap-backend/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecsTaskExecutionRoleV2"  # Cambia el nombre aquí
+  name               = "ecsTaskExecutionRoleV2"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -22,4 +22,37 @@ resource "aws_iam_role" "ecs_task_execution_role" {
       assume_role_policy,
     ]
   }
+}
+
+resource "aws_iam_policy" "ecs_task_execution_policy" {
+  name        = "ecsTaskExecutionPolicyV2"
+  description = "Policy for ECS Task Execution Role to pull images from ECR and send logs to CloudWatch"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetAuthorizationToken"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
 }
